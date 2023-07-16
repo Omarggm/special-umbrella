9a3995e066bb19b729c1ce052de0e9ba6005ec9d
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-16 10:16:44 -0700

    converted over to apollo and auth

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const contextMiddleware = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +17,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: contextMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
