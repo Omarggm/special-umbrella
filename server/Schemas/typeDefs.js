df06fa5c1059ae5886b6b4000df56114d8fd35cb
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-16 10:39:58 -0700

    added Schems folder with base structure

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Query {
+    }
+
+    type Mutation {
+    }
+    `;
+module.exports = typeDefs;
