0a4a43df6d96370407a4a8c758c25188d74891a3
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-16 17:42:58 -0700

    refresh of starter code

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const contextMiddleware = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
-
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,13 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: contextMiddleware,
-});
-
-server.applyMiddleware({ app });
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
